1e6ef3b6f1121dd0401bb5dfa550b97f4b06742b
Richard Aljaste <richard.aljaste@ut.ee>
2021-03-25 19:36:00 +0200

    EquationTest class test cases revised

--- app/src/main/java/com/example/matemaatik/EquationTest.java
+++ app/src/main/java/com/example/matemaatik/EquationTest.java
@@ -3,12 +3,12 @@ package com.example.matemaatik;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Equationtest {
+public class EquationTest {
     public static void main(String[] args) {
-        Subtract lahuta = new Subtract(0,10);
-        Add liida = new Add(0,5);
-        Multiply korruta = new Multiply(5,10);
-        Divide jaga = new Divide(0,10);
+        Subtract lahuta = new Subtract(20,2);
+        Add liida = new Add(20,2);
+        Multiply korruta = new Multiply(15,2);
+        Divide jaga = new Divide(20,2);
 
         List<Equation> tehted = new ArrayList<Equation>();
         tehted.add(lahuta);
