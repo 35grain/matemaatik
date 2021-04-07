ac6269ed8ea3028067319ef902a3de31855b6fb3
Richard A <richard.aljaste@redwall.ee>
2021-04-07 16:09:57 +0300

    GitHub repo button added

--- app/src/main/java/com/example/matemaatik/InfoActivity.java
+++ app/src/main/java/com/example/matemaatik/InfoActivity.java
@@ -3,6 +3,7 @@ package com.example.matemaatik;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -21,10 +22,24 @@ public class InfoActivity extends AppCompatActivity {
                 backToLastPage();
             }
         });
+
+        ImageButton githubRepo = (ImageButton) findViewById(R.id.githubRepo);
+        githubRepo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebPage("https://github.com/35grain/matemaatik");
+            }
+        });
     }
 
     private void backToLastPage() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void openWebPage(String url) {
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
 }
