099f39e6b1e2e0c2c745730ffe00a78cffe76e25
Richard A <richard.aljaste@redwall.ee>
2021-04-07 09:38:12 +0300

    Info view back button & description

--- app/src/main/java/com/example/matemaatik/InfoActivity.java
+++ app/src/main/java/com/example/matemaatik/InfoActivity.java
@@ -2,7 +2,10 @@ package com.example.matemaatik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        ImageButton backButton = (ImageButton) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backToLastPage();
+            }
+        });
+    }
+
+    private void backToLastPage() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
