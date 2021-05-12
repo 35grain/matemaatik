a468247c47603dc287b1b306fad37403720513cc
Richard Aljaste <richard.aljaste@ut.ee>
2021-05-12 14:57:48 +0300

    Info activity, activity history revert

--- app/src/main/java/ee/ratr/matemaatik/InfoActivity.java
+++ app/src/main/java/ee/ratr/matemaatik/InfoActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import ee.ratr.matemaatik.R;
@@ -17,6 +19,11 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
+        // removes status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // button to main activity
         ImageButton backButton = (ImageButton) findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,11 +32,12 @@ public class InfoActivity extends AppCompatActivity {
             }
         });
 
+        // button to GitHub repo
         ImageButton githubRepo = (ImageButton) findViewById(R.id.githubRepo);
         githubRepo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openWebPage("https://github.com/35grain/matemaatik");
+                openWebPage(getResources().getString(R.string.github_repo));
             }
         });
     }
